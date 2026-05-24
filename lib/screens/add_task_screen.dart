02ee35628829b196de09e1d20ebebd638d211f9a
mizanurrahmanzoy <mizan258173@gmail.com>
2026-05-25 02:43:34 +0600

    add task screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -5,6 +5,61 @@ class AddTaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add Task")),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Task Group",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextFormField(
+              maxLines: 5,
+
+              decoration: InputDecoration(
+                labelText: "Description",
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                ),
+
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+
+                child: const Text(
+                  "Add",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
